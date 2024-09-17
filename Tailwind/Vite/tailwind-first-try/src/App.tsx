e31916e46ecaddf04e20a0dd9bc19d2ee310bb32
MartotoBG <martingav123@gmail.com>
2024-09-17 18:12:30 +0300

    trying the classes

--- Tailwind/Vite/tailwind-first-try/src/App.tsx
+++ Tailwind/Vite/tailwind-first-try/src/App.tsx
@@ -1,35 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+    <div>
+      <section className="h-screen bg-green-400 flex items-center justify-center m-12 text-center text-3xl">
+        <p className="text-blue-600 p-12 md:p-14 lg:p-16 xl:p-20 2xl:p-24">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores quae aliquid optio dolores animi cupiditate temporibus fugiat dolorem reprehenderit possimus? Numquam pariatur vitae provident blanditiis, tempora expedita ducimus iste saepe?
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      </section>
+    </div>
   )
 }
 
-export default App
+export default App;
