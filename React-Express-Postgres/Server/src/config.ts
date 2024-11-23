a95dd3fde6d93f69d2ef94dbad7bda06f6c4acf5
MartotoBG <martingav123@gmail.com>
2024-11-23 13:55:43 +0200

    Deploying PostgreSQL db to Aiven

--- React-Express-Postgres/Server/src/config.ts
+++ React-Express-Postgres/Server/src/config.ts
@@ -1,4 +1,5 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 const POSTGRESS_PASSWORD = process.env.POSTGRESS_PASSWORD;
-export {POSTGRESS_PASSWORD};
+const AIVEN_POSTGRESS_PASSWORD = process.env.AIVEN_POSTGRESS_PASSWORD;
+export {POSTGRESS_PASSWORD, AIVEN_POSTGRESS_PASSWORD};
--- React-Express-Postgres/Server/src/db.ts
+++ React-Express-Postgres/Server/src/db.ts
@@ -1,11 +1,18 @@
 import { Pool } from "pg";
-import { POSTGRESS_PASSWORD } from "./config";
+import { POSTGRESS_PASSWORD, AIVEN_POSTGRESS_PASSWORD } from "./config";
+
+const isProduction = (process.env.NODE_ENV || 'development') === 'production';
+console.log(isProduction)
+// PostgreSQL configuration based on the environment
 const pool = new Pool({
-    user: "postgres",
-    password: POSTGRESS_PASSWORD,
-    host: "localhost",
-    port: 5432,
-    database: "perntodo",
-  });
-  
-  export default pool;
+  user: isProduction ? "avnadmin" : "postgres", // Use avnadmin for Aiven in production
+  password: isProduction ? AIVEN_POSTGRESS_PASSWORD : POSTGRESS_PASSWORD,
+  host: isProduction
+    ? "todo-app-pern-service-todo-app-pern.l.aivencloud.com" // Aiven host in production
+    : "localhost",
+  port: isProduction ? 23175 : 5432, // Use Aiven port in production and 5432 for local
+  database: isProduction ? "perntodo" : "perntodo", // Aiven database in production
+  ssl: isProduction ? { rejectUnauthorized: false } : false, // Enable SSL in production
+});
+
+export default pool;
